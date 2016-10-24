5c8f2586fcf7675e7fd61a757ca731f90b836445
jia <edison7476@hotmail.com>
2016-10-24 15:46:27 -0700

    Fun With Algorithms

---
+++ majority_element.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    var numsTable = {};
+    var n = Math.floor(nums.length/2);
+    var majorityElement;
+
+    if(nums.length == 1){
+        return nums[0];
+    }
+
+    for(var i = 0; i < nums.length; i++){
+       numsTable[nums[i]]  = (numsTable[nums[i]] || 0) + 1;
+        if(numsTable[nums[i]] > n ){
+            majorityElement = nums[i];
+       }
+    }
+    return majorityElement;
+};
