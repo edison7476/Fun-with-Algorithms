9aeebccd73a7ec966159e8af5574f42e5ed82771
jia <edison7476@hotmail.com>
2016-11-01 16:18:36 -0700

    Reverse a singly linked list.

---
+++ Reverse_Linked_List.js
@@ -0,0 +1,25 @@
+/*
+Reverse a singly linked list.
+*/
+
+
+var reverseList = function(head) {
+    if(!head) return null ;
+
+    else if(!head.next) return head;
+                                       // ex 1-2-3-4
+    var moveToBack = head;             // 1-2-3-4
+    var remainingList = head.next;     // 2-3-4
+    moveToBack.next = null;            // moveToBack --> 1
+
+    while(remainingList !== null){
+        var newHead = remainingList;         // 2-3-4
+        remainingList = remainingList.next;  // 3-4
+        newHead.next = moveToBack;           // newHead --> 2-1
+
+
+        moveToBack = newHead;
+    }
+
+    return newHead;
+};
